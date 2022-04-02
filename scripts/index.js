31652d08e301c64f71c11f464ce8320078bf7c82
natalai2410 <58889273+natalai2410@users.noreply.github.com>
2022-04-02 23:43:04 +0300

    Правки JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,34 @@
-const popupOpen = document.querySelector('.profile__btn-edit');
-const popupClose = document.querySelector('.popup__btn-close');
-const popupOpened = document.querySelector('.popup');
+const popupOpenButton = document.querySelector('.profile__btn-edit');
+const popupCloseButton = document.querySelector('.popup__btn-close');
+const popup = document.querySelector('.popup');
 const popupForm = document.querySelector('.popup__form');
-const avatarTitleBody = document.querySelector('.profile__title');
-const avatarSubtitleBody = document.querySelector('.profile__subtitle');
-const avatarTitle = document.getElementById('input-1');
-const avatarSubTitle = document.getElementById('input-2');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+const inputName = document.getElementById('input-1');
+const inputJob = document.getElementById('input-2');
+
 function onLoad() {
-    avatarTitle.value = avatarTitleBody.textContent;
-    avatarSubTitle.value = avatarSubtitleBody.textContent;
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
 }
+
 function onSave(e) {
     e.preventDefault();
-    avatarTitleBody.textContent = avatarTitle.value;
-    avatarSubtitleBody.textContent = avatarSubTitle.value;
-    CloseModalWindow();
+    profileName.textContent = inputName.value;
+    profileJob.textContent = inputJob.value;
+    closeModalWindow();
 }
-popupForm.addEventListener('submit', onSave);
-function OpenModalWindow() {
-    popupOpened.classList.add('popup_opened');
+
+function openModalWindow() {
+    popup.classList.add('popup_opened');
     onLoad();
 }
-function CloseModalWindow() {
-    popupOpened.classList.remove('popup_opened');
+
+function closeModalWindow() {
+    popup.classList.remove('popup_opened');
 }
-popupOpen.addEventListener('click', OpenModalWindow);
-popupClose.addEventListener('click', CloseModalWindow);
-for (let LikeBtn of document.querySelectorAll('.place-item__like')) {
-    LikeBtn.addEventListener('click', function () {
-        this.classList.toggle('place-item__like_active');
-    });
-}
+
+popupCloseButton.addEventListener('click', closeModalWindow);
+popupForm.addEventListener('submit', onSave);
+popupOpenButton.addEventListener('click', openModalWindow);
 
